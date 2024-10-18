9ae6500fc853095ebbc5e357091b84eae183b4d5
alluman <mathghost11@gmail.com>
2024-10-19 01:42:02 +0900

    www

--- deadlock/deadlock/settings.py
+++ deadlock/deadlock/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i1)g882-kvh64+aei2o_jo#uoucbib3+dus$p)*yhsl9b5=p_5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['banpick.site']
+ALLOWED_HOSTS = ['www.banpick.site']
 
 
 # Application definition
