b8a60a8e7154862ee9026ac5da9128165d8c5d97
alluman <mathghost11@gmail.com>
2024-10-20 10:47:16 +0900

    static

--- deadlock/deadlock/settings.py
+++ deadlock/deadlock/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i1)g882-kvh64+aei2o_jo#uoucbib3+dus$p)*yhsl9b5=p_5'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
